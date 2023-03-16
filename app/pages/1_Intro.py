74f86cd91f616aecffb4cd3cd2ac649c7f0f01a1
Paul Bridi <paulbridi@gmail.com>
2023-03-16 17:31:37 +0000

    Update Home and Intro pages w styling

--- app/pages/1_Intro.py
+++ app/pages/1_Intro.py
@@ -19,7 +19,7 @@ st.markdown("""
 <style>
 .stApp {
     background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
-    url("https://images.pexels.com/photos/616401/pexels-photo-616401.jpeg");
+    url("https://raw.githubusercontent.com/paulbridi/kitchen_ai_app/master/intro_bg%20_large.jpeg");
     background-size: 1500px 1000px;
     position: fixed;
 }
@@ -51,7 +51,7 @@ st.markdown("")
 st.markdown("")
 st.markdown("")
 col_a, col_b, col_c, col_d, col_e = st.columns((0.5,2,1.5,2,0.5), gap="medium")
-col_b.markdown(f'# :one:\n ## Upload an image of your fridge and our AI will identify the food ingredients inside')
+col_b.markdown(f'# :one:\n ## Upload an image of your food ingredients and our AI will identify what is inside')
 col_c.markdown(f"# :two:\n ## Specify your preferences")
 col_d.markdown(f'# :three:\n ## Our app will suggest recipes based on ingredients and preferences you provide!')
 
